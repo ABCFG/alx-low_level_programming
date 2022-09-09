5fe6bb2b619af0b3a95f9ab0db9e7821abc12ed0
ABCFG <bindeponle@gmail.com>
2022-09-09 14:18:47 -0700

    Write a script that generates the assembly code (Intel syntax) of a C code and save it in an output file.

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,16 +1,26 @@
 #include<stdio.h>
 
 /**
- * main - Entry point
+ * main - This function begins the execution of c program
  *
- * Return: Always (0) success
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	printf("size of a char: %lu byte(s)\n", (unsigned long)sizeof(char));
-	printf("size of an int: %lu byte(s)\n", (unsigned long)sizeof(int));
-	printf("size of long int: %lu byte(s)\n", (unsigned long)sizeof(long));
-	printf("size of long long int: %lu byte(s)\n", (unsigned long)sizeof(long long));
-	printf("size of float: %u byte(s)\n", (unsigned long)sizeof(float));
+	int a;
+
+	long b;
+
+	long long c;
+
+	char d;
+
+	float f;
+
+	printf("size of char: %ld byte(s)\n", sizeof(d));
+	printf("size of int : %ld byte(s)\n", sizeof(a));
+	printf("size of a long int: %ld byte(s)\n", sizeof(c));
+	printf("size of long: %ld byte(s)\n", sizeof(b));
+	printf("size of float: %ld byte(s)\n", sizeof(f));
 	return (0);
 }
