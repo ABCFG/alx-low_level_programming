53395c8bc67030117a5b63be0f3d21e52b3a16e4
ABCFG <bindeponle@gmail.com>
2022-09-11 13:13:24 -0700

    Write a program that prints all possible different combinations of two digits.

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,12 +9,15 @@ int main(void)
 {
 int l1;
 int l2;
-for (l1 = 0; l1 <= 10; l1++)
+for (l1 = 0; l1 < 10; l1++)
 
-	for (l2 = 1; l2 < 9; l2++)
+	for (l2 = 0; l2 < 10; l2++)
 	{
 		putchar(l1 % 10 + '0');
 		putchar(l2 % 10 + '0');
+
+		if (l1 == 9 && l1 == 9)
+			continue;
 
 		putchar(',');
 		putchar(' ');
