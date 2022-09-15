f0e2a1e2304d82dc15deff7f7e14f893253b2874
ABCFG <bindeponle@gmail.com>
2022-09-15 14:56:40 -0700

    This program converts a date to the day of year and determines how many days are left in the year, taking leap year into consideration.

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -1,9 +1,8 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
  * print_remaining_days - takes a date and prints how many days are
- * left in the year, taking the leap year into account
+ * left in the year, taking the leap years into account
  * @month: month in number format
  * @day: day of month
  * @year: year
